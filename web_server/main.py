80c68b81c60186c461910ffb1a72a31bd8778e1f
Oli <olifer97@gmail.com>
2021-09-18 12:55:46 -0300

    add simple counter for pages. TODO: move to another app

--- web_server/main.py
+++ web_server/main.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
+from services.visits_counter import VisitsCounter
 
 app = Flask(__name__)
+counter = VisitsCounter()
 
 @app.route('/home/')
-@app.route('/home/<name>')
-def home(name=None):
-    return render_template('home.html', name=name)
+def home():
+    count = counter.add_visit("home")
+    return render_template('home.html', counter=count)
+
+@app.route('/about/')
+def about():
+    count = counter.add_visit("about")
+    return render_template('about.html', counter=count)
+
+@app.route('/jobs/')
+def jobs():
+    count = counter.add_visit("jobs")
+    return render_template('jobs.html', counter=count)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
