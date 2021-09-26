07a48c96de2f06852869d0ef92fdeee9961a7925
Olivia <olix@Olivias-MacBook-Pro.local>
2021-09-26 10:00:10 -0300

    R3-1822 changes for uploading to google cloud

--- page_visits_counter/main.py
+++ page_visits_counter/main.py
@@ -1,17 +1,18 @@
-from flask import Flask, request, Response
+from flask import Response
 from counter import VisitsCounter
 import json
 
-app = Flask(__name__)
+#app = Flask(__name__)
 counter = VisitsCounter()
 
-@app.route('/visits', methods=['POST'])
-def add_visit():
-    body = request.json
+#@app.route('/visits', methods=['POST'])
+def add_visit(request):
+    body = request.get_json(silent=True)
+    #body = request.json
     count = counter.add_visit(body["key"])
     response_body = json.dumps({"visits": count})
     return Response(response_body, status=201, mimetype='application/json')
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5005")
+#if __name__ == '__main__':
+#    app.run(host="0.0.0.0", port="5005")
--- web_server/main.py
+++ web_server/main.py
@@ -1,23 +1,25 @@
-from flask import Flask, render_template
+#from flask import Flask, render_template
+import os
+from flask import escape, render_template
 from services.visits_counter import VisitsCounterService
 
-app = Flask(__name__)
-counter = VisitsCounterService()
+#app = Flask(__name__)
+counter = VisitsCounterService(url=os.environ['PAGE_VISITS_COUNTER_URL'])
 
-@app.route('/home/')
-def home():
+#@app.route('/home/')
+def home(request):
     count = counter.add_visit("home")
     return render_template('home.html', counter=count)
 
-@app.route('/about/')
-def about():
+#@app.route('/about/')
+def about(request):
     count = counter.add_visit("about")
     return render_template('about.html', counter=count)
 
-@app.route('/jobs/')
-def jobs():
+#@app.route('/jobs/')
+def jobs(request):
     count = counter.add_visit("jobs")
     return render_template('jobs.html', counter=count)
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000")
+#if __name__ == '__main__':
+#    app.run(host="0.0.0.0", port="5000")
