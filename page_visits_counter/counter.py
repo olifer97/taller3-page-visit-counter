a13e978c27606bb215a940aad6c6b742266b400c
Oli <olifer97@gmail.com>
2021-09-27 20:42:32 -0300

    distributed counter

--- page_visits_counter/counter.py
+++ page_visits_counter/counter.py
@@ -1,9 +1,27 @@
+from google.cloud import firestore
+
+from distributed_counters import DistributedCounter
+
+KEYS = ['home', 'about', 'jobs']
 class VisitsCounter:
     def __init__(self):
-        self.pages = {}
+        self.db = firestore.Client()
+        
+        self.counter = DistributedCounter(2)
+
+        self.docs = {}
+        col = self.db.collection("counters")
+        for key in KEYS:
+            self.docs[key] = col.document(key)
+
+        for doc_ref in self.docs.values():
+            self.counter.init_counter(doc_ref)
+        #self.pages = {}
     
     def add_visit(self, key):
-        self.pages[key] = self.pages.get(key, 0) + 1
+        self.counter.increment_counter(self.docs[key])
+        #self.pages[key] = self.pages.get(key, 0) + 1
 
     def get_visits(self, key):
-        return self.pages.get(key, 0)
+        return self.counter.get_count(self.docs[key])
+        #return self.pages.get(key, 0)
